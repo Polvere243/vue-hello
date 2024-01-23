cd937b91db916e6f415a167f34da78adb2ae0abb
Polvere243 <danielercoli@gmail.com>
2024-01-23 09:04:06 +0100

    finished exercise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("js ok");
+console.log("Vue Ok", Vue);
+const {createApp} = Vue;
+const app = createApp({
+    data() {
+        return {
+            text1: "Hello",
+            text2: "World"
+        }
+             
+        
+    }
+})
+
+app.mount ("#root");
